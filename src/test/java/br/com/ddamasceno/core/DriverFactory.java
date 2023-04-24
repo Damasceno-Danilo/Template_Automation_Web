f63ef6a80bcc615f9bb3bbfee1598d319a2523bc
Danilo Augusto <danilo.augustodama@gmail.com>
2023-04-24 18:44:23 -0300

    Atualização de comandos para criar DSL

--- src/test/java/br/com/ddamasceno/core/DriverFactory.java
+++ src/test/java/br/com/ddamasceno/core/DriverFactory.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class DriverFactory {
 
@@ -36,4 +37,5 @@ public class DriverFactory {
     public WebDriver getDriver() {
         return driver;
     }
+
 }
--- src/test/java/br/com/ddamasceno/teste/Formulario.java
+++ src/test/java/br/com/ddamasceno/teste/Formulario.java
@@ -3,8 +3,10 @@ package br.com.ddamasceno.teste;
 import br.com.ddamasceno.core.Browser;
 import br.com.ddamasceno.core.DriverFactory;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,8 +15,10 @@ import org.openqa.selenium.support.ui.Select;
 public class Formulario {
     WebDriver driver;
      DriverFactory driverFactory;
+
     @Before
     public void inicia() {
+
         driverFactory = new DriverFactory(Browser.CHROME);
         driver = driverFactory.getDriver();
         driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
@@ -22,27 +26,36 @@ public class Formulario {
 
     @After
     public void finaliza() {
-       driver.quit();
+       //driver.quit();
     }
-    @Test
-    public void clicar() {
-        driver.findElement(By.id("elementosForm:sexo:0")).click();
-    }
+
     @Test
     public void escrever() {
         driver.findElement(By.id("elementosForm:nome")).sendKeys("Danilo");
     }
     @Test
-    public void selecionarComboIndex() {
-        WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
-        Select combo = new Select(element);
-        combo.selectByIndex(3);
+    public void escreverSobrenome() {
+        driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Damasceno");
     }
     @Test
-    public void selecionarComboValue() {
+    public void clicarRadio() {
+        driver.findElement(By.id("elementosForm:sexo:0")).click();
+    }
+    @Test
+    public void clicarCheckBox() {
+        driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
+    }
+    @Test
+    public void selecionarComboPorIndex() {
         WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
         Select combo = new Select(element);
-        combo.selectByValue("superior");
+        combo.selectByIndex(4);
+    }
+    @Test
+    public void selecionarComboPorValue() {
+        WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
+        Select combo = new Select(element);
+        combo.selectByValue("mestrado");
     }
     @Test
     public void selecionarComboPorTexto() {
@@ -50,5 +63,12 @@ public class Formulario {
         Select combo = new Select(element);
         combo.selectByVisibleText("Superior");
     }
-        
+
+    @Test
+    public void interagirAlerta() {
+        driver.findElement(By.id("alert")).click();
+        Alert alert = driver.switchTo().alert();
+        Assert.assertEquals("Alert Simples", alert.getText());
+        alert.accept();
+    }
 }
