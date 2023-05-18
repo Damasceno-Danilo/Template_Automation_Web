dbac780a4cb0f954545c3be6cc6342a1f1f28317
Danilo Augusto <danilo.augustodama@gmail.com>
2023-05-17 23:49:26 -0300

    Atualização DSL

--- src/test/java/br/com/ddamasceno/teste/Formulario.java
+++ src/test/java/br/com/ddamasceno/teste/Formulario.java
@@ -1,99 +1,74 @@
 package br.com.ddamasceno.teste;
 
 import br.com.ddamasceno.core.Browser;
+import br.com.ddamasceno.core.DSL;
 import br.com.ddamasceno.core.DriverFactory;
-import org.junit.*;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
 
 public class Formulario {
     WebDriver driver;
      DriverFactory driverFactory;
+     private DSL dsl;
 
     @Before
     public void iniciarNavegador() {
-
         driverFactory = new DriverFactory(Browser.CHROME);
         driver = driverFactory.getDriver();
         driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+        dsl = new DSL(driver);
     }
     @After
     public void fecharNavegador() {
         //driver.quit();
     }
-
     @Test
-    public void escrever() {
-        driver.findElement(By.id("elementosForm:nome")).sendKeys("Danilo");
+    public void escreverNome() {
+        dsl.escrever("elementosForm:nome", "Danilo");
     }
     @Test
     public void escreverSobrenome() {
-        driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Damasceno");
+        dsl.escrever("elementosForm:sobrenome", "Damasceno");
     }
     @Test
     public void clicarRadio() {
-        driver.findElement(By.id("elementosForm:sexo:0")).click();
+        dsl.clicarCheckBox("elementosForm:sexo:0");
     }
     @Test
-    public void clicarCheckBox() {
-        driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
+    public void interagirCheckBox() {
+        dsl.clicarCheckBox("elementosForm:comidaFavorita:0");
     }
     @Test
     public void selecionarComboPorIndex() {
-        WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
-        Select combo = new Select(element);
-        combo.selectByIndex(4);
+        dsl.selecionarPorIndex( "elementosForm:escolaridade", 4);
     }
     @Test
     public void selecionarComboPorValue() {
-        WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
-        Select combo = new Select(element);
-        combo.selectByValue("mestrado");
+        dsl.selecionarComboPorValue("elementosForm:escolaridade", "Mestrado");
     }
     @Test
     public void selecionarComboPorTexto() {
-        WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
-        Select combo = new Select(element);
-        combo.selectByVisibleText("Superior");
+        dsl.selecionarComboPorTexto("elementosForm:escolaridade", "Superior");
     }
     @Test
     public void interagirAlerta() {
-        driver.findElement(By.id("alert")).click();
-        Alert alert = driver.switchTo().alert();
-        Assert.assertEquals("Alert Simples", alert.getText());
-        alert.accept();
+        dsl.interagirAlerta("alert", "Alert Simples");
     }
 
     @Test
-    public void interagirAlertaConfim() {
-        driver.findElement(By.id("confirm")).click();
-        Alert alert = driver.switchTo().alert();
-        Assert.assertEquals("Confirm Simples", alert.getText());
-        alert.accept();
-        Assert.assertEquals("Confirmado", alert.getText());
-        alert.accept();
+    public void alertaConfim() {
+        dsl.interagirAlertaConfim("confirm", "Confirm Simples", "Confirmado");
+    }
 
-        driver.findElement(By.id("confirm")).click();
-        alert = driver.switchTo().alert();
-        Assert.assertEquals("Confirm Simples", alert.getText());
-        alert.dismiss();
-        Assert.assertEquals("Negado", alert.getText());
-        alert.dismiss();
+    @Test
+    public void alertaNegado() {
+        dsl.intergirAlertaNegado("confirm", "Confirm Simples", "Negado");
     }
 
     @Test
     public void interagirAlertaPrompt() {
-        driver.findElement(By.id("prompt")).click();
-        Alert alert = driver.switchTo().alert();
-        Assert.assertEquals("Digite um numero", alert.getText());
-        alert.sendKeys("13");
-        alert.accept();
-        Assert.assertEquals("Era 13?", alert.getText());
-        alert.accept();
-        Assert.assertEquals(":D", alert.getText());
-        alert.accept();
+        dsl.interagirAlertaPrompt("prompt");
     }
 }
