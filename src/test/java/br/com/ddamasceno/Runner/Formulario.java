cded4ef17a2b386b4333656bdc4c84b4cfefb69a
unknown <danilo.augustodama@gmail.com>
2024-08-06 09:36:36 -0300

    Refactor: Updated DSL class to WebAction and created page objects

--- src/test/java/br/com/ddamasceno/Runner/Formulario.java
+++ src/test/java/br/com/ddamasceno/Runner/Formulario.java
@@ -1,7 +1,7 @@
 package br.com.ddamasceno.teste;
 
 import br.com.ddamasceno.core.Browser;
-import br.com.ddamasceno.core.DSL;
+import br.com.ddamasceno.core.WebActions;
 import br.com.ddamasceno.core.DriverFactory;
 import org.junit.After;
 import org.junit.Before;
@@ -11,14 +11,14 @@ import org.openqa.selenium.WebDriver;
 public class Formulario {
     WebDriver driver;
      DriverFactory driverFactory;
-     private DSL dsl;
+     private WebActions webActions;
 
     @Before
     public void iniciarNavegador() {
         driverFactory = new DriverFactory(Browser.CHROME);
         driver = driverFactory.getDriver();
         driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-        dsl = new DSL(driver);
+        webActions = new WebActions(driver);
     }
     @After
     public void fecharNavegador() {
@@ -26,49 +26,49 @@ public class Formulario {
     }
     @Test
     public void escreverNome() {
-        dsl.escrever("elementosForm:nome", "Danilo");
+        webActions.escrever("elementosForm:nome", "Danilo");
     }
     @Test
     public void escreverSobrenome() {
-        dsl.escrever("elementosForm:sobrenome", "Damasceno");
+        webActions.escrever("elementosForm:sobrenome", "Damasceno");
     }
     @Test
     public void clicarRadio() {
-        dsl.clicarCheckBox("elementosForm:sexo:0");
+       webActions.clicarCheckBox("elementosForm:sexo:0");
     }
     @Test
     public void interagirCheckBox() {
-        dsl.clicarCheckBox("elementosForm:comidaFavorita:0");
+        webActions.clicarCheckBox("elementosForm:comidaFavorita:0");
     }
     @Test
     public void selecionarComboPorIndex() {
-        dsl.selecionarPorIndex( "elementosForm:escolaridade", 4);
+        webActions.selecionarPorIndex( "elementosForm:escolaridade", 4);
     }
     @Test
     public void selecionarComboPorValue() {
-        dsl.selecionarComboPorValue("elementosForm:escolaridade", "Mestrado");
+        webActions.selecionarComboPorValue("elementosForm:escolaridade", "Mestrado");
     }
     @Test
     public void selecionarComboPorTexto() {
-        dsl.selecionarComboPorTexto("elementosForm:escolaridade", "Superior");
+        webActions.selecionarComboPorTexto("elementosForm:escolaridade", "Superior");
     }
     @Test
     public void interagirAlerta() {
-        dsl.interagirAlerta("alert", "Alert Simples");
+        webActions.interagirAlerta("alert", "Alert Simples");
     }
 
     @Test
     public void alertaConfim() {
-        dsl.interagirAlertaConfim("confirm", "Confirm Simples", "Confirmado");
+        webActions.interagirAlertaConfim("confirm", "Confirm Simples", "Confirmado");
     }
 
     @Test
     public void alertaNegado() {
-        dsl.intergirAlertaNegado("confirm", "Confirm Simples", "Negado");
+        webActions.intergirAlertaNegado("confirm", "Confirm Simples", "Negado");
     }
 
     @Test
     public void interagirAlertaPrompt() {
-        dsl.interagirAlertaPrompt("prompt");
+        webActions.interagirAlertaPrompt("prompt");
     }
 }
